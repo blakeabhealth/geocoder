7c1ea4d39033f94e720cc63d7db6dce11629e50d
blakeabhealth <77858999+blakeabhealth@users.noreply.github.com>
2022-03-15 16:07:38 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,8 +2,7 @@
 apt-get update &&
 apt-get install unixodbc-dev -y &&
 apt-get install g++ -y &&
-curl https://packages.microsoft.com/keys/microsoft.asc &&
-apt-key add -y &&
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - &&
 curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list &&
 apt-get update &&
 ACCEPT_EULA=Y apt-get install -y msodbcsql18 &&
