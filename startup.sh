e443cba8300c997634d6d4744fbdb8d77e67d860
blakeabhealth <77858999+blakeabhealth@users.noreply.github.com>
2022-03-15 16:15:48 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,6 +6,9 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - &&
 curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list &&
 apt-get update &&
 ACCEPT_EULA=Y apt-get install -y msodbcsql18 &&
+ACCEPT_EULA=Y apt-get install -y mssql-tools18 &&
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc &&
+source ~/.bashrc
 cd /mnt/geocode &&
 pip install -r requirements.txt &&
 python ./geocoder-10-threads-public.py
