c1a6d1dbd5ae1424157b1d5a2ee68cbce09b2aa9
blakeabhealth <77858999+blakeabhealth@users.noreply.github.com>
2022-03-15 12:29:00 -0400

    swap to windows

---
+++ startup.bat
@@ -0,0 +1,1 @@
+cd /mnt/geocode && pip install -r requirements.txt && python geocoder-10-threads-public.py
--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt-get update && cd /mnt/geocode && pip install -r requirements.txt && python ./geocoder-10-threads-public.py
-#this was added to force a permission change
