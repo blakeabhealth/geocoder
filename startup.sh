3a1e7fa320a020ac37d59d24d4fdd83a72096a51
blakeabhealth <77858999+blakeabhealth@users.noreply.github.com>
2022-03-15 14:36:18 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-apt-get update && cd /mnt/geocode && pip install -r requirements.txt && python ./geocoder-10-threads-public.py
-#this was added to force a permission change
+apt-get update && apt-get install unixodbc-dev -y && apt-get install g++ -y && cd /mnt/geocode && pip install -r requirements.txt && python ./geocoder-10-threads-public.py
